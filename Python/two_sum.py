8e86eeeaf72324ffda50ecae6d51403b7e849c68
Drew Fink <me@drewf.ink>
2024-01-21 23:31:17 -0600

    Make my two sum solutions more efficient

--- Python/two_sum.py
+++ Python/two_sum.py
@@ -1,21 +1,12 @@
-# Runtime: Embarrassing
-# Memory: 18MB (51.32%)
-
 from typing import List
 
 def twoSum(nums: List[int], target: int) -> List[int]:
-    for i in nums:
-        for j in nums:
-            if nums.index(i) != nums.index(j) and i + j == target:
-                return [nums.index(i), nums.index(j)]
-
-def twoSumTwo(nums: List[int], target: int) -> List[int]:
     length = len(nums)
     for i in range(0, length, 1):
-        for j in range(0, length, 1):
-            if i != j and nums[i] + nums[j] == target:
+        for j in range(i + 1, length, 1):
+            if nums[i] + nums[j] == target:
                 return [i, j]
 
-print(twoSumTwo([2,7,11,15], 9))
-print(twoSumTwo([3,2,4], 6))
-print(twoSumTwo([3,3], 6))
+print(twoSum([2,7,11,15], 9))
+print(twoSum([3,2,4], 6))
+print(twoSum([3,3], 6))
